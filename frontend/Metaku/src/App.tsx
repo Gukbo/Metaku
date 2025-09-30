70ed392c4a59fab5579a75158807bed31435b612
Gukbo <2onesound@naver.com>
2025-09-30 21:10:59 +0900

    feat) 라우팅 설정

--- frontend/Metaku/src/App.tsx
+++ frontend/Metaku/src/App.tsx
@@ -1,11 +1,22 @@
 import "./App.css";
 import Layout from "./components/Layout";
 import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import BoardDetail from "./pages/BoardDetail";
+import BoardList from "./pages/BoardList";
+import BoardWrite from "./pages/BoardWrite";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <Routes>
-      <Route element={<Layout />}></Route>
+      <Route element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="/BoardList" element={<BoardList />} />
+        <Route path="/BoardDetail" element={<BoardDetail />} />
+        <Route path="/BoardWrite" element={<BoardWrite />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
     </Routes>
   );
 }
---
+++ frontend/Metaku/src/pages/BoardWrite.tsx
@@ -0,0 +1,7 @@
+export default function BoardWrite() {
+  return (
+    <div>
+      <h1>홈</h1>
+    </div>
+  );
+}
